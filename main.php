28d5b1274e34b2a94ed722fdb713f54614d0a597
resteem <eremeevst@gmail.com>
2014-11-07 21:15:28 +0300

    First version.

--- main.php
+++ main.php
@@ -1,79 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
 	<title>Воронеж в Инстаграме</title>
 </head>
 <body>
-	<h1>Что сфотографировали в Воронеже за последние сутки</h1>
-<?php
-	include_once('_/config.php');
-
-	if (!function_exists('showArray'))
-	{
-		function showArray($value)
-		{
-			echo '<pre>', print_r($value, TRUE), '</pre>';
-		}
-	} // showArray
-
-	// Draw circles on Google maps - http://www.mapdevelopers.com/draw-circle-tool.php
-	$arCircles5000_VORONEZH = array(
-		array( // center
-			'LAT' => '51.66448760893037',
-			'LNG' => '39.226183673046876',
-		),
-		array( // up-left center
-			'LAT' => '51.724973',
-			'LNG' => '39.257229',
-		),
-		array( // soviet
-			'LAT' => '51.597998109841136',
-			'LNG' => '39.132799883984376',
-		),
-		array( // komintern
-			'LAT' => '51.67726287930439',
-			'LNG' => '39.1286800109375',
-		),
-		array( // lenin
-			'LAT' => '51.74193555156363',
-			'LNG' => '39.15339924921875',
-		),
-		array( // left
-			'LAT' => '51.60738073741625',
-			'LNG' => '39.25776936640625',
-		),
-	);
+	<h1 class="logo">Что сфотографировали в Воронеже за последние сутки</h1>
+<?
+	include_once('_/auth_configs.php');
+	include_once('lib/functions.php');
+	include_once('lib/city_russia_36.php');
 
 	$now = time();
 	$arParams = array(
 		'DISTANCE' => '5000',
-		'MIN_TIMESTAMP' => $now - (1 * 24 * 60 * 60), // yesterday
+		'MIN_TIMESTAMP' => $now - (60 * 60 * 24), // yesterday
 		'MAX_TIMESTAMP' => $now,
+		'INSTAGRAM_ACCESS_TOKEN' => $INSTAGRAM_ACCESS_TOKEN,
 	);
-	$tplUrlLocationCircle = 'https://api.instagram.com/v1/media/search?access_token=' . $INSTAGRAM_ACCESS_TOKEN .
-			'&distance=' . $arParams['DISTANCE'] .
-			'&min_timestamp=' . $arParams['MIN_TIMESTAMP'] .
-			'&max_timestamp=' . $arParams['MAX_TIMESTAMP'] .
-			'&lat=%LAT%&lng=%LNG%';
-	foreach ($arCircles5000_VORONEZH as $arPoint)
-	{
-		$url = str_replace(array('%LAT%', '%LNG%'), array($arPoint['LAT'], $arPoint['LNG']), $tplUrlLocationCircle);
-		$oData = json_decode(file_get_contents($url));
-		if ($oData->meta->code == 200)
-		{
-			showArray($oData->data); exit;
-			/*
-			foreach ($oData->data as $oPhoto)
-			{
-				?>
-					<a href="<?php echo $oPhoto->link;?>" title="<?php echo $oPhoto->user->username;?>">
-						<img src="<?php echo $oPhoto->images->thumbnail->url;?>">
-					</a>
-				<?php
-			}
-			*/
-		}
-	}
+	$tplUrlLocationCircle = getTplUrlLocation($arParams);
+
+	$arPhotos = getLocationPhotos($arCityRussia36, $tplUrlLocationCircle);
 ?>
+	<div class="items"><?
+		foreach ($arPhotos as $arPhoto):
+		?><div class="item left">
+			<div class="info">
+				<div class="author left">
+					<div class="left">
+						<a href="<?=$arPhoto['USER']['PROFILE_URL'];?>" title="<?=$arPhoto['USER']['FULL_NAME'];?>">
+							<img src="<?=$arPhoto['USER']['PROFILE_PICTURE'];?>" alt="<?=$arPhoto['USER']['USERNAME'];?>">
+						</a>
+					</div>
+					<div class="left">
+						<a class="uline" href="<?=$arPhoto['USER']['PROFILE_URL'];?>" title="<?=($arPhoto['USER']['FULL_NAME'] ? $arPhoto['USER']['FULL_NAME'] : $arPhoto['USER']['USERNAME']);?>">
+							<?=$arPhoto['USER']['USERNAME'];?>
+						</a>
+						<div class="location">
+							<a class="uline" href="<?=$arPhoto['LOCATION']['LINK'];?>" title="<?=($arPhoto['LOCATION']['NAME'] ? $arPhoto['LOCATION']['NAME'] : $arPhoto['LOCATION']['NAME_BY_COORD']);?>">
+								Location
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="right">
+					<div class="time right" title="<?=date('d.m.Y H:i:s', $arPhoto['OBJECT']['CREATED_TIME']);?>">
+						<?=date('H:i', $arPhoto['OBJECT']['CREATED_TIME']);?>
+					</div>
+					<div class="clear"></div>
+					<div class="stats right">
+						<div class="comments right"><?=$arPhoto['OBJECT']['COMMENTS_COUNT']?></div>
+						<div class="likes right"><?=$arPhoto['OBJECT']['LIKES_COUNT']?></div>
+					</div>
+				</div>
+			</div>
+			<div class="photo">
+				<a href="<?=$arPhoto['OBJECT']['LINK'];?>" title="<?=$arPhoto['OBJECT']['CAPTION'];?>">
+					<img src="<?=$arPhoto['OBJECT']['URL_LOW']?>">
+				</a>
+			</div>
+		</div><?
+		endforeach;
+	?></div>
 </body>
 </html>
